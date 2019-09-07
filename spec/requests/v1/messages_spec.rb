b8a75777723c12febeee466fdf267b16ed7912e7
Sameh AbdelZaher <sameh.iv@gmail.com>
2019-09-07 10:58:03 +0200

    message show endpoint

--- spec/requests/v1/messages_spec.rb
+++ spec/requests/v1/messages_spec.rb
@@ -17,4 +17,22 @@ RSpec.describe 'V1::Messages', type: :request do
       expect(json_response[:data][:items].count).to eq(5)
     end
   end
+
+  describe 'GET /application/:token/chats/:number/messages/:number' do
+    let(:message){ create :message }
+    before do
+      get v1_application_chat_message_path(message.chat.application.token, message.chat.number, message.number),
+          params: { page: 1, per_page: 5 }, headers: { accept: :json }
+    end
+
+    it 'return code 200' do
+      expect(json_response[:code]).to eq(200)
+    end
+
+    it 'return message' do
+      expect(json_response[:data][:number]).to eq(message.number)
+    end
+
+  end
+
 end
